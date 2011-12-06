5649682d6e945e92d31ffff2b86b1b166ea1f606
Skywave <thakoch@gmail.com>
2011-12-06 18:05:26 +0100

    rough fixes to get ril working

--- device/zte/blade/device_blade.mk
+++ device/zte/blade/device_blade.mk
@@ -156,6 +156,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.com.android.dateformat=dd-MM-yyyy \
     ro.ril.hsxpa=2 \
     ro.ril.gprsclass=10 \
+    ro.telephony.ril.v3=icccardstatus,datacall,signalstrength,facilitylock \
     ro.build.baseband_version=P729BB01 \
     ro.telephony.default_network=0 \
     ro.telephony.call_ring.multiple=false
---
+++ hardware/ril/rild/Android.mk
@@ -0,0 +1,39 @@
+# Copyright 2006 The Android Open Source Project
+
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES:= \
+	rild.c
+
+
+LOCAL_SHARED_LIBRARIES := \
+	libcutils \
+	libril
+
+ifeq ($(TARGET_ARCH),arm)
+LOCAL_SHARED_LIBRARIES += libdl
+endif # arm
+
+LOCAL_CFLAGS := -DRIL_SHLIB
+
+LOCAL_MODULE:= rild
+
+include $(BUILD_EXECUTABLE)
+
+# For radiooptions binary
+# =======================
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES:= \
+	radiooptions.c
+
+LOCAL_SHARED_LIBRARIES := \
+	libcutils \
+
+LOCAL_CFLAGS := \
+
+LOCAL_MODULE:= radiooptions
+LOCAL_MODULE_TAGS := debug
+
+include $(BUILD_EXECUTABLE)
