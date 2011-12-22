897102c19e8ec0904ef2c15024812306e572b5a4
Tom Brennan <tommie.brennan@gmail.com>
2011-12-22 14:58:40 +0000

    Pushed up correct timezone fix

--- libcore/luni/src/main/java/libcore/util/ZoneInfoDB.java
+++ libcore/luni/src/main/java/libcore/util/ZoneInfoDB.java
@@ -13,9 +13,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
- 
+
 package libcore.util;
- 
+
 import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.nio.ByteBuffer;
@@ -28,10 +28,10 @@ import java.util.List;
 import java.util.TimeZone;
 import libcore.io.IoUtils;
 import libcore.io.MemoryMappedFile;
- 
+
 // TODO: repackage this class, used by frameworks/base.
 import org.apache.harmony.luni.internal.util.TimezoneGetter;
- 
+
 /**
  * A class used to initialize the time zone database.  This implementation uses the
  * 'zoneinfo' database as the source of time zone information.  However, to conserve
@@ -47,22 +47,22 @@ public final class ZoneInfoDB {
      */
     private static final String ZONE_DIRECTORY_NAME =
             System.getenv("ANDROID_ROOT") + "/usr/share/zoneinfo/";
- 
+
     /**
      * The name of the file containing the concatenated time zone records.
      */
     private static final String ZONE_FILE_NAME = ZONE_DIRECTORY_NAME + "zoneinfo.dat";
- 
+
     /**
      * The name of the file containing the index to each time zone record within
      * the zoneinfo.dat file.
      */
     private static final String INDEX_FILE_NAME = ZONE_DIRECTORY_NAME + "zoneinfo.idx";
- 
+
     private static final Object LOCK = new Object();
- 
+
     private static final String VERSION = readVersion();
- 
+
     /**
      * The 'ids' array contains time zone ids sorted alphabetically, for binary searching.
      * The other two arrays are in the same order. 'byteOffsets' gives the byte offset
@@ -75,11 +75,11 @@ public final class ZoneInfoDB {
     static {
         readIndex();
     }
- 
+
     private static ByteBuffer mappedData = mapData();
- 
+
     private ZoneInfoDB() {}
- 
+
     /**
      * Reads the file indicating the database version in use.  If the file is not
      * present or is unreadable, we assume a version of "2007h".
@@ -97,7 +97,7 @@ public final class ZoneInfoDB {
             IoUtils.closeQuietly(versionFile);
         }
     }
- 
+
     /**
      * Traditionally, Unix systems have one file per time zone. We have one big data file, which
      * is just a concatenation of regular time zone files. To allow random access into this big
@@ -112,23 +112,23 @@ public final class ZoneInfoDB {
         RandomAccessFile indexFile = null;
         try {
             indexFile = new RandomAccessFile(INDEX_FILE_NAME, "r");
- 
+
             // The database reserves 40 bytes for each id.
             final int SIZEOF_TZNAME = 40;
             // The database uses 32-bit (4 byte) integers.
             final int SIZEOF_TZINT = 4;
- 
+
             byte[] idBytes = new byte[SIZEOF_TZNAME];
- 
+
             int numEntries = (int) (indexFile.length() / (SIZEOF_TZNAME + 3*SIZEOF_TZINT));
- 
+
             char[] idChars = new char[numEntries * SIZEOF_TZNAME];
             int[] idEnd = new int[numEntries];
             int idOffset = 0;
- 
+
             byteOffsets = new int[numEntries];
             rawUtcOffsets = new int[numEntries];
- 
+
             for (int i = 0; i < numEntries; i++) {
                 indexFile.readFully(idBytes);
                 byteOffsets[i] = indexFile.readInt();
@@ -137,7 +137,7 @@ public final class ZoneInfoDB {
                     throw new AssertionError("length in index file < sizeof(tzhead)");
                 }
                 rawUtcOffsets[i] = indexFile.readInt();
- 
+
                 // Don't include null chars in the String
                 int len = idBytes.length;
                 for (int j = 0; j < len; j++) {
@@ -146,10 +146,10 @@ public final class ZoneInfoDB {
                     }
                     idChars[idOffset++] = (char) (idBytes[j] & 0xFF);
                 }
- 
+
                 idEnd[i] = idOffset;
             }
- 
+
             // We create one string containing all the ids, and then break that into substrings.
             // This way, all ids share a single char[] on the heap.
             String allIds = new String(idChars, 0, idOffset);
@@ -163,7 +163,7 @@ public final class ZoneInfoDB {
             IoUtils.closeQuietly(indexFile);
         }
     }
- 
+
     /**
      * Rather than open, read, and close the big data file each time we look up a time zone,
      * we map the big data file during startup, and then just use the ByteBuffer.
@@ -185,7 +185,7 @@ public final class ZoneInfoDB {
             IoUtils.closeQuietly(file);
         }
     }
- 
+
     private static TimeZone makeTimeZone(String id) throws IOException {
         // Work out where in the big data file this time zone is.
         int index = Arrays.binarySearch(ids, id);
@@ -193,34 +193,34 @@ public final class ZoneInfoDB {
             return null;
         }
         int start = byteOffsets[index];
- 
+
         // We duplicate the ByteBuffer to allow unsynchronized access to this shared data,
         // despite Buffer's implicit position.
         ByteBuffer data = mappedData.duplicate();
         data.position(start);
- 
+
         // Variable names beginning tzh_ correspond to those in "tzfile.h".
         // Check tzh_magic.
         if (data.getInt() != 0x545a6966) { // "TZif"
             return null;
         }
- 
+
         // Skip the uninteresting part of the header.
         data.position(start + 32);
- 
+
         // Read the sizes of the arrays we're about to read.
         int tzh_timecnt = data.getInt();
         int tzh_typecnt = data.getInt();
         int tzh_charcnt = data.getInt();
- 
+
         int[] transitions = new int[tzh_timecnt];
         for (int i = 0; i < tzh_timecnt; ++i) {
             transitions[i] = data.getInt();
         }
- 
+
         byte[] type = new byte[tzh_timecnt];
         data.get(type);
- 
+
         int[] gmtOffsets = new int[tzh_typecnt];
         byte[] isDsts = new byte[tzh_typecnt];
         byte[] abbreviationIndexes = new byte[tzh_typecnt];
@@ -229,18 +229,18 @@ public final class ZoneInfoDB {
             isDsts[i] = data.get();
             abbreviationIndexes[i] = data.get();
         }
- 
+
         byte[] abbreviationList = new byte[tzh_charcnt];
         data.get(abbreviationList);
- 
+
         return new ZoneInfo(id, transitions, type, gmtOffsets, isDsts,
                 abbreviationIndexes, abbreviationList);
     }
- 
+
     public static String[] getAvailableIDs() {
         return (String[]) ids.clone();
     }
- 
+
     public static String[] getAvailableIDs(int rawOffset) {
         List<String> matches = new ArrayList<String>();
         for (int i = 0, end = rawUtcOffsets.length; i < end; i++) {
@@ -250,7 +250,7 @@ public final class ZoneInfoDB {
         }
         return matches.toArray(new String[matches.size()]);
     }
- 
+
     public static TimeZone getSystemDefault() {
         synchronized (LOCK) {
             TimezoneGetter tzGetter = TimezoneGetter.getInstance();
@@ -266,7 +266,7 @@ public final class ZoneInfoDB {
             return TimeZone.getTimeZone(zoneName);
         }
     }
- 
+
     public static TimeZone getTimeZone(String id) {
         if (id == null) {
             return null;
@@ -277,7 +277,7 @@ public final class ZoneInfoDB {
             return null;
         }
     }
- 
+
     public static String getVersion() {
         return VERSION;
     }
